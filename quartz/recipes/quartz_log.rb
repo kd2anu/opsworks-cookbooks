d3e62583efa98e232927dc40e74eab3675ac2d5c
kd2anu <shawn.ko@scanbuy.com>
2016-06-10 02:22:15 +0000

    change dir

--- quartz/recipes/quartz_log.rb
+++ quartz/recipes/quartz_log.rb
@@ -1,27 +1,27 @@
 directory '/var/log/coupons' do
-  owner 'tomcat'
-  group 'tomcat'
+  owner 'root'
+  group 'root'
   mode '0766'
   action :create
 end
 
 file '/var/log/coupons/api2POS.log' do
-  owner 'tomcat'
-  group 'tomcat'
+  owner 'root'
+  group 'root'
   mode '0644'
   action :create
 end
 
 file '/var/log/coupons/quartz.log' do
-  owner 'tomcat'
-  group 'tomcat'
+  owner 'root'
+  group 'root'
   mode '0644'
   action :create
 end
 
 file '/var/log/coupons/trazasCupones.log' do
-  owner 'tomcat'
-  group 'tomcat'
+  owner 'root'
+  group 'root'
   mode '0644'
   action :create
 end
