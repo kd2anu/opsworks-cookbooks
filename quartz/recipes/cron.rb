592ae6c3697178e513244b266d3d6797d32ae35e
kd2anu <shawn.ko@scanbuy.com>
2016-06-10 15:51:30 +0000

    missing /webapps dir

--- quartz/recipes/cron.rb
+++ quartz/recipes/cron.rb
@@ -1,7 +1,7 @@
 application = node[:opsworks][:instance][:hostname].chop
 
 template 'quartz_jobs.xml' do
-  path ::File.join(node['tomcat']['install_dir'],"#{application}",'/WEB-INF/classes/quartz_jobs.xml')
+  path ::File.join(node['tomcat']['install_dir'],'webapps',"#{application}",'/WEB-INF/classes/quartz_jobs.xml')
   source 'quartz_jobs.xml.erb'
   owner 'deploy'
   group 'apache'
