7f71e5cdf6d4615be151b9b13d38b9fbcbd36a52
kd2anu <shawn.ko@scanbuy.com>
2016-06-15 21:30:17 +0000

    change quartz config

--- quartz/recipes/cron.rb
+++ quartz/recipes/cron.rb
@@ -1,12 +1,14 @@
 application = node[:opsworks][:instance][:hostname].chop
 
-template 'quartz_jobs.xml' do
-  path ::File.join(node['tomcat']['install_dir'],'webapps',"#{application}",'/WEB-INF/classes/quartz_jobs.xml')
-  source 'quartz_jobs.xml.erb'
-  owner 'deploy'
-  group 'apache'
-  mode 0640
-  variables({
-     :minute => node[:opsworks][:instance][:hostname][-1,1]
-  })
+if application == "api2campaign"
+  template 'quartz.properties.erb' do
+    path ::File.join(node['tomcat']['install_dir'],'webapps',"#{application}",'/WEB-INF/classes/quartz.properties')
+    source 'quartz_jobs.xml.erb'
+    owner 'deploy'
+    group 'apache'
+    mode 0640
+    variables({
+       :minute => Integer(node[:opsworks][:instance][:hostname][-1,1])*5
+    })
+  end
 end
