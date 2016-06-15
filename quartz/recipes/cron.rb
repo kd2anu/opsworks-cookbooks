b46db0e1f6a2e6c9140158932afc4e551241037c
kd2anu <shawn.ko@scanbuy.com>
2016-06-15 21:33:50 +0000

    fix error

--- quartz/recipes/cron.rb
+++ quartz/recipes/cron.rb
@@ -3,7 +3,7 @@ application = node[:opsworks][:instance][:hostname].chop
 if application == "api2campaign"
   template 'quartz.properties.erb' do
     path ::File.join(node['tomcat']['install_dir'],'webapps',"#{application}",'/WEB-INF/classes/quartz.properties')
-    source 'quartz_jobs.xml.erb'
+    source 'quartz.properties.erb'
     owner 'deploy'
     group 'apache'
     mode 0640
