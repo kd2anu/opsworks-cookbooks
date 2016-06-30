4cbac1c11acde35db0963c87584a8eab85d8d972
kd2anu <shawn.ko@scanbuy.com>
2016-06-30 03:07:52 +0000

    modified for yum install

--- nrpe/recipes/default.rb
+++ nrpe/recipes/default.rb
@@ -1,44 +1,21 @@
-user node[:nrpe][:user] do
-  action :create
-  system true
-  shell "/bin/false"
+package 'nrpe' do
+  action :install
 end
 
-directory node[:nrpe][:dir] do
-  owner "icinga"
-  mode "0755"
-  action :create
+template '/etc/nagios/nrpe.cfg' do
+  source 'nrpe.cfg.erb'
+  owner 'root'
+  group 'root'
+  mode '0754'
 end
 
 remote_directory "/usr/local/nagios" do
   source "nrpe_files"
-  owner "icinga"
-  group "icinga"
+  owner "nrpe"
+  group "nrpe"
   mode "0755"
   recursive true
   action :create_if_missing
-end
-
-execute "fix_ownership" do
-  command "chown -R icinga:icinga /usr/local/nagios/*"
-  user "root"
-end
-
-execute "fix_permission" do
-  command "chmod -R ug+x /usr/local/nagios/*"
-  user "root"
-end
-
-template "/etc/init.d/nrpe" do
-  source "nrpe.erb"
-  mode "0755"
-  owner "root"
-  group "root"
-end
-
-bash "add_service" do
-  code "sudo chkconfig --add nrpe"
-  not_if "chkconfig --list |grep -i nrpe"
 end
 
 service "nrpe" do
