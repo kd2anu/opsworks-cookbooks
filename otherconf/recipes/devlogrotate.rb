971aeb598571f5a51e9c0120fc435957628afd95
kd2anu <shawn.ko@scanbuy.com>
2016-07-07 04:46:33 +0000

    add logrotate config recipes

---
+++ otherconf/recipes/devlogrotate.rb
@@ -0,0 +1,28 @@
+conf_dir="/opt/logrotate.d"
+number=node[:opsworks][:instance][:hostname][-1,1]
+apps=['api2pos','api2campaignmgr','campaignmgr','api2coupons','mycoupons']
+
+template "/etc/crontab" do
+  source "crontab.erb"
+  owner "root"
+  mode "0744"
+end
+
+directory "#{conf_dir}" do
+  owner "root"
+  group "root"
+  mode "0744"
+  action :create
+end
+
+apps.each do |eachapp|
+  template "#{conf_dir}/#{eachapp}.conf" do
+    source "dev.conf.erb"
+    owner "root"
+    mode "0744"
+    variables({
+      :APPNAME => "#{eachapp}",
+      :NUMBER => "#{number}"
+    })
+  end
+end
