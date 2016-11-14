5b085f7ccab6467305141bc8b0c2a855f4ddb90d
kd2anu <shawn.ko@scanbuy.com>
2016-11-14 15:26:54 +0000

    add flushram alias to onoffline

--- otherconf/recipes/onoffline.rb
+++ otherconf/recipes/onoffline.rb
@@ -9,3 +9,10 @@ template "/usr/bin/offline" do
   owner "root"
   mode "0755"
 end
+
+execute "flushram" do
+  command '/bin/echo "alias flushram=\'free -m && sync && echo 3 > /proc/sys/vm/drop_caches && free -m\'" >> ~/.bashrc'
+  user "root"
+  group "root"
+  not_if 'grep "flushram" ~/.bashrc'
+end
